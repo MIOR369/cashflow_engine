18e4f9335f4fe4bf5a62dfd27bff586d5519e2a2
ratiomoralis <ratiomoralis@example.com>
2026-04-12 05:42:16 +0200

    build script for render

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+g++ main.cpp parser.cpp simulator.cpp simulator_prob.cpp delay_engine.cpp multi_cause_engine.cpp impact_engine.cpp date_utils.cpp delay_utils.cpp risk_engine.cpp decision_engine.cpp advice_engine.cpp scenario_engine.cpp optimizer_engine.cpp structure_engine.cpp execution_engine.cpp margin.cpp final_engine.cpp -O3 -std=c++17 -o cashflow
